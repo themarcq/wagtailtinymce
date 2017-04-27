d6b03c35b525215a1c05c7995b7c3379054079c7
Maciej Fic <maciej.fic@jmr.pl>
2017-04-27 17:24:11 +0200

    list styles

--- wagtailtinymce/static/wagtailtinymce/js/tinymce-editor.js
+++ wagtailtinymce/static/wagtailtinymce/js/tinymce-editor.js
@@ -63,8 +63,8 @@ function makeTinyMCEEditable(id, kwargs) {
             editor.contentStyles.push('img.richtext-image.full-width {width: 100%; height: auto;}');
             editor.contentStyles.push('ul.arrow {list-style-type: none;margin-left: -15px;}');
             editor.contentStyles.push('ul.tick {list-style-type: none;margin-left: -15px;}');
-            editor.contentStyles.push('.arrow li::before {content: "\\2B9A";}');
-            editor.contentStyles.push('.tick li::before {content: "\\1F5F8";}');
+            editor.contentStyles.push('.arrow li::before {content: "\\2B9A";font-family: arial;}');
+            editor.contentStyles.push('.tick li::before {content: "\\1F5F8";font-family: arial;}');
         }
     });
 
